c70f0956320d2efc7175150f61fa5f4e2b93bf21
Quýt Tiến <tiennqhe170733@fpt.edu.vn>
2025-03-24 01:23:08 +0700

    create layout staff and for application (routes)

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+import Page404 from "../features/page404";
+import AccountPage from "../features/staff/Account";
+import CartPage from "../features/staff/Cart";
+import CollectionPage from "../features/staff/Collection";
+import HomePage from "../features/staff/Home";
+import LoginPage from "../features/staff/Login";
+import ProductPage from "../features/staff/Product";
+import RegisterPage from "../features/staff/Register";
+import { adminRoutes } from "./admin";
+
+const basicRoutes = [
+  { path: "/", element: HomePage },
+  { path: "/login", element: LoginPage },
+  { path: "/register", element: RegisterPage },
+  { path: "/collection", element: CollectionPage },
+  { path: "/product", element: ProductPage },
+  { path: "/cart", element: CartPage },
+  { path: "/account", element: AccountPage },
+  { path: "/*", element: Page404 },
+];
+
+export { basicRoutes, adminRoutes };
